7041de74bd736c4bca00d26d825cbd00999dc500
Lokkilol <87026453+Lokkilol@users.noreply.github.com>
2025-03-22 00:37:52 -0400

    Initializing Spring Boot Application

---
+++ src/main/java/jwu/swetask/SwetaskApplication.java
@@ -0,0 +1,13 @@
+package jwu.swetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwetaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwetaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jwu/swetask/SwetaskApplicationTests.java
@@ -0,0 +1,13 @@
+package jwu.swetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwetaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
